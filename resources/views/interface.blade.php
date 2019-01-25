f0a22aab8238940a848fff406d0a13fba7ef1585
Mr Chimp <chimpytk@gmail.com>
2019-01-25 23:52:39 +0000

    Add logout link

--- resources/views/interface.blade.php
+++ resources/views/interface.blade.php
@@ -10,6 +10,15 @@
         <button>Run</button>
     </form> -->
     <p>
-        Logged in as: {{ Auth::user()->name }}
+        Logged in as: {{ Auth::user()->name }} -
+        <a href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+            document.getElementById('logout-form').submit();">
+            {{ __('Logout') }}
+        </a>
     </p>
+
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+    </form>
 @endsection
