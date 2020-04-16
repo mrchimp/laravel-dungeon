c089b3486f382efad49391890687c8bc00c28f8f
Jake Gully <chimpytk@gmail.com>
2020-04-16 16:56:02 +0100

    Fix seeders for portal changes

--- database/seeds/RoomSeeder.php
+++ database/seeds/RoomSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Dungeon\Portal;
 use Dungeon\Room;
 use Illuminate\Database\Seeder;
 
@@ -12,26 +13,39 @@ class RoomSeeder extends Seeder
      */
     public function run()
     {
-        $north_room = Room::create([
+        $north_room = factory(Room::class)->create([
             'description' => 'You are in the start room. There is not a lot here.',
         ]);
 
-        $south_room = Room::create([
+        $south_room = factory(Room::class)->create([
             'description' => 'You are in the second room. You feel a profound sense of achievement for having made it here.',
         ]);
 
-        $east_room = Room::create([
+        $east_room = factory(Room::class)->create([
             'description' => 'Oh my god another room. Does this maze ever end? Yes it does. This is the last room.',
         ]);
 
-        $north_room->setSouthExit($south_room, [
-            'description' => 'A wooden door.',
-        ]);
+        $north_south_portal = factory(Portal::class)->create();
 
-        $south_room->setEastExit($east_room, [
-            'description' => 'A sturdy door with a code lock',
+        $north_room->setSouthExit(
+            $south_room,
+            [
+                'description' => 'A wooden door.',
+                'portal_id' => $north_south_portal->id
+            ]
+        );
+
+        $south_east_portal = factory(Portal::class)->create([
             'locked' => true,
             'code' => 1234,
         ]);
+
+        $south_room->setEastExit(
+            $east_room,
+            [
+                'description' => 'A sturdy door with a code lock',
+                'portal_id' => $south_east_portal->id,
+            ]
+        );
     }
 }
