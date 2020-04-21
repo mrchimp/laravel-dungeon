6a0ff9468ef5626b5c4658d6dc0afb1c48aaca43
Jake Gully <chimpytk@gmail.com>
2020-04-21 17:26:10 +0100

    Don't include all of lodash. That would be crazy.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,3 @@
-window._ = require('lodash');
-
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
@@ -21,9 +19,7 @@ let token = document.head.querySelector('meta[name="csrf-token"]');
 if (token) {
   window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
-  console.error(
-    'CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token'
-  );
+  console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
 /**
