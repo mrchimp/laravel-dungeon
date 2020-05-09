d1b905712605e55060900758185c2d60fbf4a6ba
Jake Gully <chimpytk@gmail.com>
2020-05-09 01:30:48 +0100

    Tidy upviews

--- resources/views/interface.blade.php
+++ resources/views/interface.blade.php
@@ -1,20 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-  <div id="app">
-    <dungeon-interface></dungeon-interface>
-  </div>
-{{--
-  <p>
-    Logged in as: {{ Auth::user()->name }} -
-    <a href="{{ route('logout') }}"
-      onclick="event.preventDefault();
-      document.getElementById('logout-form').submit();">
-      {{ __('Logout') }}
-    </a>
-  </p>
-
-  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-    @csrf
-  </form> --}}
+  <dungeon-interface></dungeon-interface>
 @endsection
